813a64bd9450258ad6261382ad49e5bf3263ea47
xiehaojun <2879981462@qq.com>
2025-02-22 21:30:35 +0800

    修改了登录与注册的过渡效果

--- src/views/login/loginPage.vue
+++ src/views/login/loginPage.vue
@@ -123,108 +123,110 @@ const handleLogin = () => {
       </div>
     </header>
     <!-- 主体内容填写 -->
-    <main class="flex-1 flex justify-center items-center">
+    <main class="flex-1 flex justify-center items-center overflow-hidden">
       <!-- 登录主体内容填写 -->
-      <div v-if="isLogin" class="px-6 bg-primary rounded-2xl min-w-min py-4">
-        <div class="ml-6 mb-2 mt-6">
-          <p class="tracking-wide text-black text-3xl font-semibold">
-            欢迎登陆
-          </p>
-          <p class="tracking-wide text-black text-3xl font-semibold mt-2">
-            国际中文教育平台
-          </p>
-        </div>
-        <div class="flex flex-col items-center justify-center">
-          <div class="flex items-center mt-4 w-full">
-            <optionsCp
-              text="我是学生"
-              :isActive="isActive === '我是学生'"
-              @click="handleIsActive"
-            >
-              <img
-                class="w-10 h-10"
-                src="@/assets/icon/student.png"
-                alt="我是学生"
-              />
-            </optionsCp>
-            <optionsCp
-              text="我是老师"
-              @click="handleIsActive"
-              :isActive="isActive === '我是老师'"
-            >
-              <img
-                class="w-12 h-12"
-                src="@/assets/icon/teacher.png"
-                alt="我是老师"
-              />
-            </optionsCp>
-            <optionsCp
-              text="我是管理员"
-              @click="handleIsActive"
-              :isActive="isActive === '我是管理员'"
-            >
-              <img
-                class="w-10 h-10"
-                src="@/assets/icon/admin.png"
-                alt="我是管理员"
-              />
-            </optionsCp>
+      <transition name="slide" mode="out-in">
+        <div v-if="isLogin" class="px-6 bg-primary rounded-2xl min-w-min py-4">
+          <div class="ml-6 mb-2 mt-6">
+            <p class="tracking-wide text-black text-3xl font-semibold">
+              欢迎登陆
+            </p>
+            <p class="tracking-wide text-black text-3xl font-semibold mt-2">
+              国际中文教育平台
+            </p>
           </div>
-          <div class="w-11/12 rounded-md mt-4 flex flex-col items-center">
-            <form @submit.prevent="handleLogin" class="w-full">
-              <!-- 账号输入框 -->
-              <InputComponent
-                v-model="formData.email"
-                placeholder="请输入邮箱"
-                id="email"
-                label="邮箱"
-                :error="errors.email"
-              />
-
-              <!-- 密码输入框 -->
-              <InputComponent
-                type="password"
-                placeholder="请输入密码"
-                label="密码"
-                v-model="formData.password"
-                :error="errors.password"
-                id="password"
-              />
-            </form>
-            <!-- 忘记密码 -->
-            <a href="#" class="text-black text-sm">忘记密码？</a>
-            <!-- 我已同意 -->
-            <div class="flex items-center mt-2">
-              <input type="checkbox" class="mr-2" />
-              <p class="text-sm">我已阅读并同意</p>
-              <a href="#" class="text-blue-500 text-sm">《用户服务协议》</a>
-              <p class="text-sm">和</p>
-              <a href="#" class="text-blue-500 text-sm">《隐私协议》</a>
+          <div class="flex flex-col items-center justify-center">
+            <div class="flex items-center mt-4 w-full">
+              <optionsCp
+                text="我是学生"
+                :isActive="isActive === '我是学生'"
+                @click="handleIsActive"
+              >
+                <img
+                  class="w-10 h-10"
+                  src="@/assets/icon/student.png"
+                  alt="我是学生"
+                />
+              </optionsCp>
+              <optionsCp
+                text="我是老师"
+                @click="handleIsActive"
+                :isActive="isActive === '我是老师'"
+              >
+                <img
+                  class="w-12 h-12"
+                  src="@/assets/icon/teacher.png"
+                  alt="我是老师"
+                />
+              </optionsCp>
+              <optionsCp
+                text="我是管理员"
+                @click="handleIsActive"
+                :isActive="isActive === '我是管理员'"
+              >
+                <img
+                  class="w-10 h-10"
+                  src="@/assets/icon/admin.png"
+                  alt="我是管理员"
+                />
+              </optionsCp>
             </div>
-            <MyButton
-              type="primary"
-              class="mt-4 w-36 py-2 text-black"
-              @click="handleLogin"
-              >登录</MyButton
-            >
-            <!-- 没有账号，点击注册 -->
-            <a
-              href="#"
-              @click="isLogin = false"
-              class="text-black text-sm mt-4 underline"
-              >没有账号？点击注册</a
-            >
-            <p class="text-sm font-thin mt-4">GCEP V1.0</p>
+            <div class="w-11/12 rounded-md mt-4 flex flex-col items-center">
+              <form @submit.prevent="handleLogin" class="w-full">
+                <!-- 账号输入框 -->
+                <InputComponent
+                  v-model="formData.email"
+                  placeholder="请输入邮箱"
+                  id="email"
+                  label="邮箱"
+                  :error="errors.email"
+                />
+
+                <!-- 密码输入框 -->
+                <InputComponent
+                  type="password"
+                  placeholder="请输入密码"
+                  label="密码"
+                  v-model="formData.password"
+                  :error="errors.password"
+                  id="password"
+                />
+              </form>
+              <!-- 忘记密码 -->
+              <a href="#" class="text-black text-sm">忘记密码？</a>
+              <!-- 我已同意 -->
+              <div class="flex items-center mt-2">
+                <input type="checkbox" class="mr-2" />
+                <p class="text-sm">我已阅读并同意</p>
+                <a href="#" class="text-blue-500 text-sm">《用户服务协议》</a>
+                <p class="text-sm">和</p>
+                <a href="#" class="text-blue-500 text-sm">《隐私协议》</a>
+              </div>
+              <MyButton
+                type="primary"
+                class="mt-4 w-36 py-2 text-black"
+                @click="handleLogin"
+                >登录</MyButton
+              >
+              <!-- 没有账号，点击注册 -->
+              <a
+                href="#"
+                @click="isLogin = false"
+                class="text-black text-sm mt-4 underline"
+                >没有账号？点击注册</a
+              >
+              <p class="text-sm font-thin mt-4">GCEP V1.0</p>
+            </div>
           </div>
         </div>
-      </div>
-      <!-- 注册主体填写 -->
-      <div
-        v-else
-        class="bg-primary rounded-2xl w-2/3 py-4 flex items-center justify-center"
-      >
-        <registerComponent></registerComponent>
-      </div>
+        <!-- 注册主体填写 -->
+        <div
+          v-else
+          class="bg-primary rounded-2xl w-2/3 py-4 flex items-center justify-center"
+        >
+          <registerComponent></registerComponent>
+        </div>
+      </transition>
     </main>
   </div>
 </template>
@@ -239,5 +241,28 @@ const handleLogin = () => {
 .bg-primary1 {
   background-color: rgba(223, 242, 255, 0.5); /* 半透明背景 */
   backdrop-filter: blur(10px); /* 毛玻璃效果 */
+}
+
+/* 定义滑出和滑入的过渡样式 */
+.slide-enter-active,
+.slide-leave-active {
+  transition:
+    transform 0.5s ease,
+    opacity 0.5s ease;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(100%); /* 从右边滑出 */
+}
+
+.slide-enter-to {
+  opacity: 1;
+}
+
+.slide-leave-from {
+  opacity: 1;
+  transform: translateX(0%); /* 初始状态，不需要移动 */
 }
 </style>
